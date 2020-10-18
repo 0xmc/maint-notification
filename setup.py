b1fa4913e99d630a8abb30f2439ce8f1bdd8b115
Michael Costello <mcostello@netflix.com>
2020-10-18 10:17:38 -0700

    bump version and remove vim swap file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xmaintnote",
-    version="0.0.1_rc2",
+    version="0.0.2_rc1",
     packages=find_packages(),
     install_requires=[
         'icalendar>=3.0',
