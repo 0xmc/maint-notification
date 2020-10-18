ffca901ed5d991bf501167ebb5ebf42aeb0d90d1
Michael Costello <mcostello@netflix.com>
2020-10-18 10:58:41 -0700

    more jira requirement to setup. move dev requirements to tests as they're all test requirements.

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="xmaintnote",
-    version="0.0.2_rc1",
+    version="0.0.2rc1",
     packages=find_packages(),
     install_requires=[
-        'icalendar>=3.0',
+        'icalendar>=4.0.7',
+        'jira>=2.0.0'
     ],
 )
